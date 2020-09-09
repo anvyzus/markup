c0ad1689176cd5fd5b9681047c33c6ec557415a1
Mykyta <46813354+anvyzus@users.noreply.github.com>
2020-09-09 22:06:01 +0300

    push with footer
    push with footer block

---
+++ mtest/scripts/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let menu = document.querySelector('.menu');
+let menuButton = document.querySelector('.menu__button');
+let menuList = document.querySelector('.menu__list');
+let scrollPosition = 0;
+
+/*Fix Firefox bug*/
+scrollPosition = window.scrollY;
+
+if (scrollPosition > 100) {
+    menu.style.background = '#24292E';
+}
+/*End*/
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('open'); 
+    menuList.classList.toggle('show');
+});
+
+window.addEventListener('scroll', () => {
+    scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) {
+        menu.style.background = '#24292E';
+    } else {
+        menu.style.background = 'transparent';
+    }
+});
