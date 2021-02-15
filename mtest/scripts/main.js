93206d377edc220bb84a33ded4a7f29fa1a4be26
Mykyta <46813354+bolelod@users.noreply.github.com>
2021-02-15 12:39:36 +0200

    update
    update menu

--- mtest/scripts/main.js
+++ mtest/scripts/main.js
@@ -1,63 +1,20 @@
 'use strict';
 
-// let menu = document.querySelector('.menu');
-// let menuButton = document.querySelector('.menu__button');
-// let menuList = document.querySelector('.menu__list');
-// let scrollPosition = 0;
-
-// /*Fix Firefox bug*/
-// scrollPosition = window.scrollY;
-
-// if (scrollPosition > 100) {
-//   menu.style.background = '#24292E';
-// }
-// /*End*/
-
-// menuButton.addEventListener('click', () => {
-//   menuButton.classList.toggle('open'); 
-//   menuList.classList.toggle('show');
-// });
-
-// window.addEventListener('scroll', () => {
-//   scrollPosition = window.scrollY;
-
-//   if (scrollPosition > 100) {
-//     menu.style.background = '#24292E';
-//   } else {
-//     menu.style.background = 'transparent';
-//   }
-// });
-
 function openMenu() {
   menuButton.classList.toggle('menu__button--activated');
   menuList.classList.toggle('menu__list--shown');
+  menuPopup.classList.toggle('menu__popup--visible');
+
   if (menuList.style.transition === 'transform 0.2s ease-in-out 0s') {
     setTimeout(() => menuList.removeAttribute('style'), 200);
   } else {
     menuList.style.transition = 'transform ease-in-out 0.2s';
   }
-  // } else {
-  //   menuList.removeAttribute('style');
-  // }
-  // if (document.querySelector('.menu').offsetWidth < 768) {
-
-  // }
-  // if (menuList.style.transition === 'transform 0.2s ease-in-out 0s') {
-  //   menuList.style.transition = 'transform 0s ease-in-out 0.2s';
-  // } else {
-  //   menuList.style.transition = 'transform 0.2s ease-in-out 0s';
-  // }
-  // if (menuList.style.transition === 'transform 0.2s ease-in-out 0s') {
-  //   // setTimeout(() => menuList.removeAttribute('style'), 0);
-  //   tr
-  //   // menuList.removeAttribute('style');
-  // } else {
-  //   menuList.style.transition = 'transform ease-in-out 0.2s';
-  // }
-  // transition: transform ease-in-out 0.2s;
 }
 
 let menuButton = document.querySelector('.menu__button');
 let menuList = document.querySelector('.menu__list');
+let menuPopup = document.querySelector('.menu__popup');
 
 menuButton.addEventListener('click', openMenu);
+menuPopup.addEventListener('click', openMenu);
