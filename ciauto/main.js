748ea8a202d72e808c96600481e52556320fe4f8
Mykyta <46813354+bolelod@users.noreply.github.com>
2021-03-13 15:11:09 +0200

    upd
    update

--- ciauto/main.js
+++ ciauto/main.js
@@ -3,8 +3,19 @@
 const button = document.querySelector('.header__button');
 const main = document.querySelector('.main');
 
-function changeColor() {
-  main.classList.toggle('red')
+function createElem() {
+  let div = document.createElement('div');
+
+  div.className = "elem";
+  div.innerHTML = "New element";
+
+  main.after(div);
 }
 
-button.addEventListener('click', changeColor);
+button.addEventListener('click', createElem);
+
+// function changeColor() {
+//   main.classList.toggle('red');
+// }
+
+// button.addEventListener('click', changeColor);
