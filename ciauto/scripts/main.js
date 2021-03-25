b37747238cd3451e4ad7d3440caad59adf260729
Mykyta <46813354+bolelod@users.noreply.github.com>
2021-03-25 11:35:29 +0200

    pre last updt
    pre last update

--- ciauto/scripts/main.js
+++ ciauto/scripts/main.js
@@ -1,5 +1,41 @@
 'use strict';
 
+function smoothScroll(event) {
+  event.preventDefault();
+
+  let targetId = event.currentTarget.getAttribute("href") === "#" ? "header" : event.currentTarget.getAttribute("href");
+  let targetPosition = document.querySelector(targetId).offsetTop;
+  let startPosition = window.pageYOffset;
+  let distance = targetPosition - startPosition;
+  let duration = 700;
+  let start = null;
+  
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+
+    let progress = timestamp - start;
+
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+
+  function easeInOutCubic(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t * t + b;
+    t -= 2;
+    return c / 2 * (t * t * t + 2) + b;
+  };
+}
+function navbarLinkClick(event) {
+  smoothScroll(event);
+
+  if (menuButton.classList.contains('menu__button--opened')) {
+    openMenu();
+  }
+}
 function openMenu() {
   menuBackground.classList.toggle('menu__background--visible');
   menuButton.classList.toggle('menu__button--opened');
@@ -13,9 +49,18 @@ function closeMenu() {
   }
 }
 
-const menuBackground = document.querySelector('.menu__background');
-const menuButton = document.querySelector('.menu__button');
-const menuList = document.querySelector('.menu__list');
+let menuBackground = document.querySelector('.menu__background');
+let menuButton = document.querySelector('.menu__button');
+let menuList = document.querySelector('.menu__list');
+let navbarLinks = document.querySelectorAll('.menu__link');
+let headerButton = document.querySelector('.header__link-button--top');
 
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", navbarLinkClick);
+}
+
+headerButton.addEventListener('click', navbarLinkClick);
 menuButton.addEventListener('click', openMenu);
 menuBackground.addEventListener('click', closeMenu);
+
+
