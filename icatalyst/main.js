69f4c341fa1cdb3583578ac3ad491b44a4583f67
Mykyta <46813354+bolelod@users.noreply.github.com>
2021-04-06 12:41:51 +0300

    plu
    pre last update

--- icatalyst/main.js
+++ icatalyst/main.js
@@ -1,66 +1,5 @@
 'use strict';
 
-function openMenu() {
-  menuBackground.classList.toggle('menu__background--visible');
-  menuButton.classList.toggle('menu__button--opened');
-  menuList.classList.toggle('menu__list--opened');
-}
-function closeMenu() {
-  if (menuBackground.classList.contains('menu__background--visible')) {
-    openMenu();
-  } else {
-    return;
-  }
-}
-
-const headerButton = document.querySelector('.header__link');
-const menuBackground = document.querySelector('.menu__background');
-const menuButton = document.querySelector('.menu__button');
-const menuList = document.querySelector('.menu__list');
-const navbarLinks = document.querySelectorAll('.menu__link');
-
-// headerButton.addEventListener('click', navbarLinkClick);
-menuBackground.addEventListener('click', closeMenu);
-menuButton.addEventListener('click', openMenu);
-
-
-
-// function smoothScroll(event) {
-//   event.preventDefault();
-
-//   let targetId = event.currentTarget.getAttribute("href") === "#" ? "header" : event.currentTarget.getAttribute("href");
-//   let targetPosition = document.querySelector(targetId).offsetTop;
-//   let startPosition = window.pageYOffset;
-//   let distance = targetPosition - startPosition;
-//   let duration = 700;
-//   let start = null;
-  
-//   window.requestAnimationFrame(step);
-
-//   function step(timestamp) {
-//     if (!start) start = timestamp;
-
-//     let progress = timestamp - start;
-
-//     window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-
-//     if (progress < duration) window.requestAnimationFrame(step);
-//   }
-
-//   function easeInOutCubic(t, b, c, d) {
-//     t /= d / 2;
-//     if (t < 1) return c / 2 * t * t * t + b;
-//     t -= 2;
-//     return c / 2 * (t * t * t + 2) + b;
-//   };
-// }
-// function navbarLinkClick(event) {
-//   smoothScroll(event);
-
-//   if (menuButton.classList.contains('menu__button--opened')) {
-//     openMenu();
-//   }
-// }
 // function openMenu() {
 //   menuBackground.classList.toggle('menu__background--visible');
 //   menuButton.classList.toggle('menu__button--opened');
@@ -74,16 +13,74 @@ menuButton.addEventListener('click', openMenu);
 //   }
 // }
 
-// let menuBackground = document.querySelector('.menu__background');
-// let menuButton = document.querySelector('.menu__button');
-// let menuList = document.querySelector('.menu__list');
-// let navbarLinks = document.querySelectorAll('.menu__link');
-// let headerButton = document.querySelector('.header__link-button--top');
+// const headerButton = document.querySelector('.header__link');
+// const menuBackground = document.querySelector('.menu__background');
+// const menuButton = document.querySelector('.menu__button');
+// const menuList = document.querySelector('.menu__list');
+// const navbarLinks = document.querySelectorAll('.menu__link');
 
-// for (let i = 0; i < navbarLinks.length; i++) {
-//   navbarLinks[i].addEventListener("click", navbarLinkClick);
-// }
-
-// headerButton.addEventListener('click', navbarLinkClick);
-// menuButton.addEventListener('click', openMenu);
 // menuBackground.addEventListener('click', closeMenu);
+// menuButton.addEventListener('click', openMenu);
+
+function smoothScroll(event) {
+  event.preventDefault();
+
+  let targetId = event.currentTarget.getAttribute("href") === "#" ? "header" : event.currentTarget.getAttribute("href");
+  let targetPosition = document.querySelector(targetId).offsetTop;
+  let startPosition = window.pageYOffset;
+  let distance = targetPosition - startPosition;
+  let duration = 700;
+  let start = null;
+  
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+
+    let progress = timestamp - start;
+
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+
+  function easeInOutCubic(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t * t + b;
+    t -= 2;
+    return c / 2 * (t * t * t + 2) + b;
+  };
+}
+function navbarLinkClick(event) {
+  smoothScroll(event);
+
+  if (menuButton.classList.contains('menu__button--opened')) {
+    openMenu();
+  }
+}
+function openMenu() {
+  menuBackground.classList.toggle('menu__background--visible');
+  menuButton.classList.toggle('menu__button--opened');
+  menuList.classList.toggle('menu__list--opened');
+}
+function closeMenu() {
+  if (menuBackground.classList.contains('menu__background--visible')) {
+    openMenu();
+  } else {
+    return;
+  }
+}
+
+let menuBackground = document.querySelector('.menu__background');
+let menuButton = document.querySelector('.menu__button');
+let menuList = document.querySelector('.menu__list');
+let navbarLinks = document.querySelectorAll('.menu__link');
+let headerButton = document.querySelector('.header__link');
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", navbarLinkClick);
+}
+
+headerButton.addEventListener('click', navbarLinkClick);
+menuButton.addEventListener('click', openMenu);
+menuBackground.addEventListener('click', closeMenu);
